a6d4a5ac9c528b7ae02c1e6e61090127e5de330f
Peter Yu <2057325+yukw777@users.noreply.github.com>
2021-05-26 10:32:05 -0400

    wip data.py

--- dgu/data.py
+++ dgu/data.py
@@ -1,7 +1,10 @@
 import itertools
+import json
 
-from typing import List, Iterator
-from torch.utils.data import Sampler
+from dgu.graph import TextWorldGraph
+from dataclasses import dataclass
+from typing import List, Iterator, Dict, Any, Tuple, OrderedDict, Set
+from torch.utils.data import Sampler, Dataset
 
 
 class TemporalDataBatchSampler(Sampler[List[int]]):
@@ -36,3 +39,150 @@ class TemporalDataBatchSampler(Sampler[List[int]]):
 
     def __len__(self) -> int:
         return self.len
+
+
+class TWCmdGenDataset(Dataset):
+    """
+    TextWorld Command Generation temporal graph event dataset.
+
+    Each data point contains the following information:
+        {
+            "game": "game name",
+            "step": [walkthrough step, random step],
+            "observation": "observation...",
+            "previous_action": "previous action...",
+            "event_seq": [graph event, ...],
+            "node_labels": [node label, ...],
+            "edge_labels": [edge label, ...],
+        }
+
+    Node and edge labels are for the graph AFTER the given event sequence.
+
+    There are four event types: node addtion/deletion and edge addition/deletion.
+    Each node event contains the following information:
+        {
+            "type": "node-{add,delete}",
+            "node_id": id for node to be added/deleted,
+            "timestamp": timestamp for the event,
+        }
+    Each edge event contains the following information:
+        {
+            "type": "edge-{add,delete}",
+            "src_id": id for src node to be added/deleted,
+            "dst_id": id for dst node to be added/deleted,
+            "timestamp": timestamp for the event,
+        }
+    """
+
+    def __init__(self, data: List[Dict[str, Any]]):
+        self.data = data
+
+    def __getitem__(self, idx: int) -> Dict[str, Any]:
+        return self.data[idx]
+
+    def __len__(self) -> int:
+        return len(self.data)
+
+    @classmethod
+    def from_cmd_gen_data(cls, path: str) -> "TWCmdGenDataset":
+        with open(path, "r") as f:
+            raw_data = json.load(f)
+            graph_index = json.loads(raw_data["graph_index"])
+        data: List[Dict[str, Any]] = []
+        for example in raw_data["examples"]:
+            data.append(
+                {
+                    "game": example["game"],
+                    "step": example["step"],
+                    "observation": example["observation"],
+                    "previous_action": example["previous_action"],
+                }
+            )
+        return cls(data)
+
+    @staticmethod
+    def transform_commands_to_events(
+        cmds: List[str],
+        step: List[int],
+        graph: TextWorldGraph,
+    ) -> List[Dict[str, Any]]:
+        # timestamp is the sum of the walkthrough step and the random step
+        timestamp = sum(step)
+        events: List[Dict[str, Any]] = []
+        for cmd in cmds:
+            cmd_type, src, dst, rel = cmd.split(" , ")
+            if cmd_type == "add":
+                if graph.has_edge(src, dst):
+                    # the edge already exists, continue
+                    continue
+                # the edge doesn't exist, so add it
+                # if src or dst doesn't exit, add it first.
+                if not graph.has_node(src):
+                    graph.add_node(src)
+                    events.append(
+                        {
+                            "type": "node-add",
+                            "node_id": graph.get_node_id(src),
+                            "timestamp": timestamp,
+                        }
+                    )
+                if not graph.has_node(dst):
+                    graph.add_node(dst)
+                    events.append(
+                        {
+                            "type": "node-add",
+                            "node_id": graph.get_node_id(dst),
+                            "timestamp": timestamp,
+                        }
+                    )
+
+                # add the edge add event
+                graph.add_edge(src, dst, rel)
+                events.append(
+                    {
+                        "type": "edge-add",
+                        "src_id": graph.get_node_id(src),
+                        "dst_id": graph.get_node_id(dst),
+                        "timestamp": timestamp,
+                    }
+                )
+            elif cmd_type == "delete":
+                if not graph.has_edge(src, dst):
+                    # the edge doesn't exist, continue
+                    continue
+
+                # delete the edge and add event
+                graph.remove_edge(src, dst)
+                src_id = graph.get_node_id(src)
+                dst_id = graph.get_node_id(dst)
+                events.append(
+                    {
+                        "type": "edge-delete",
+                        "src_id": src_id,
+                        "dst_id": dst_id,
+                        "timestamp": timestamp,
+                    }
+                )
+                # if there are no edges, delete the nodes
+                if graph.in_degree(src_id) == 0 and graph.out_degree(src_id) == 0:
+                    graph.remove_node(src)
+                    events.append(
+                        {
+                            "type": "node-delete",
+                            "node_id": src_id,
+                            "timestamp": timestamp,
+                        }
+                    )
+                if graph.in_degree(dst_id) == 0 and graph.out_degree(dst_id) == 0:
+                    graph.remove_node(dst)
+                    events.append(
+                        {
+                            "type": "node-delete",
+                            "node_id": dst_id,
+                            "timestamp": timestamp,
+                        }
+                    )
+            else:
+                raise ValueError(f"Unknown command {cmd}")
+
+        return events
--- tests/test_data.py
+++ tests/test_data.py
@@ -1,6 +1,7 @@
 import pytest
 
-from dgu.data import TemporalDataBatchSampler
+from dgu.data import TemporalDataBatchSampler, TWCmdGenDataset
+from dgu.graph import TextWorldGraph
 
 
 @pytest.mark.parametrize(
@@ -18,3 +19,28 @@ def test_temporal_data_batch_sampler(batch_size, event_seq_lens, expected_batche
     sampler = TemporalDataBatchSampler(batch_size, event_seq_lens)
     assert list(iter(sampler)) == expected_batches
     assert len(sampler) == len(expected_batches)
+
+
+@pytest.mark.parametrize(
+    "cmds,steps,expected_events,expected_node_labels,expected_edge_labels",
+    [
+        (
+            ["add , player , kitchen , in"],
+            [0, 0],
+            [
+                {"type": "node-add", "node_id": 0, "timestamp": 0},
+                {"type": "node-add", "node_id": 1, "timestamp": 0},
+                {"type": "edge-add", "src_id": 0, "dst_id": 1, "timestamp": 0},
+            ],
+            [],
+            [],
+        )
+    ],
+)
+def test_tw_cmd_gen_dataset_transform_cmd_events(
+    cmds, steps, expected_events, expected_node_labels, expected_edge_labels
+):
+    g = TextWorldGraph()
+    assert (
+        TWCmdGenDataset.transform_commands_to_events(cmds, steps, g) == expected_events
+    )
