Fix masking for TemporalGraphNetwork.message()
We now mask messages for special events as well as messages
for destination nodes for node events.

dgu/nn/temporal_graph.py
@@ -28,11 +28,15 @@ class TemporalGraphNetwork(nn.Module):
         dst_ids: torch.Tensor,
         dst_mask: torch.Tensor,
         event_embeddings: torch.Tensor,
+        event_mask: torch.Tensor,
         event_timestamps: torch.Tensor,
     ) -> Tuple[torch.Tensor, torch.Tensor]:
         """
         Calculate graph event messages. We concatenate the event type,
         source and destination memories, time embedding and event embedding.
+
+        Special events like pad, start, end are masked out. Node events are also
+        masked out for destination node messages.
 
         event_type_ids: (event_seq_len)
         src_ids: (event_seq_len)
@@ -40,6 +44,7 @@ class TemporalGraphNetwork(nn.Module):
         dst_ids: (event_seq_len)
         dst_mask: (event_seq_len)
         event_embeddings: (event_seq_len, hidden_dim)
+        event_mask: (event_seq_len)
         event_timestamps: (event_seq_len)
 
         output:
@@ -53,13 +58,11 @@ class TemporalGraphNetwork(nn.Module):
         # use the memory for node embeddings
         src_embs = torch.stack([self.memory[src_id] for src_id in src_ids.tolist()]).to(
             src_ids.device
-        )
-        src_embs *= src_mask.unsqueeze(-1)
+        ) * src_mask.unsqueeze(-1)
         # (event_seq_len, hidden_dim)
         dst_embs = torch.stack([self.memory[dst_id] for dst_id in dst_ids.tolist()]).to(
             dst_ids.device
-        )
-        dst_embs *= dst_mask.unsqueeze(-1)
+        ) * dst_mask.unsqueeze(-1)
         # (event_seq_len, hidden_dim)
 
         # fetch last update timestamps
@@ -74,36 +77,45 @@ class TemporalGraphNetwork(nn.Module):
 
         # multiply src_last_update and dst_last_update by dst_mask so that we
         # only subtract last update timestamps for edge events
-        # then pass them through the time encoder to get the embeddings
+        # then pass them through the time encoder to get the embeddings.
+        # after that we mask out special events that don't require timestamps
         src_timestamp_emb = self.time_encoder(
             event_timestamps - src_last_update * dst_mask
         )
         # (event_seq_len, hidden_dim)
+        # mask out node events
         dst_timestamp_emb = self.time_encoder(
             event_timestamps - dst_last_update * dst_mask
         )
         # (event_seq_len, hidden_dim)
 
-        src_messages = torch.cat(
-            [
-                event_type_embs,
-                src_embs,
-                dst_embs,
-                src_timestamp_emb,
-                event_embeddings,
-            ],
-            dim=1,
+        # mask out special events
+        src_messages = (
+            torch.cat(
+                [
+                    event_type_embs,
+                    src_embs,
+                    dst_embs,
+                    src_timestamp_emb,
+                    event_embeddings,
+                ],
+                dim=1,
+            )
+            * event_mask.unsqueeze(-1)
         )
         # (event_seq_len, 5 * hidden_dim)
-        dst_messages = torch.cat(
-            [
-                event_type_embs,
-                dst_embs,
-                src_embs,
-                dst_timestamp_emb,
-                event_embeddings,
-            ],
-            dim=1,
+        dst_messages = (
+            torch.cat(
+                [
+                    event_type_embs,
+                    dst_embs,
+                    src_embs,
+                    dst_timestamp_emb,
+                    event_embeddings,
+                ],
+                dim=1,
+            )
+            * dst_mask.unsqueeze(-1)
         )
         # (event_seq_len, 5 * hidden_dim)
         return src_messages, dst_messages

tests/nn/test_temporal_graph.py
@@ -1,34 +1,117 @@
 import pytest
-import random
 import torch
+import torch.nn as nn
 
 from dgu.nn.temporal_graph import TemporalGraphNetwork
-from dgu.constants import EVENT_TYPES
+from dgu.nn.utils import compute_masks_from_event_type_ids
+from dgu.constants import EVENT_TYPE_ID_MAP
 
 
 @pytest.mark.parametrize(
-    "hidden_dim,event_seq_len,num_nodes,max_timestamp",
+    "num_nodes,event_type_ids,src_ids,dst_ids,event_embeddings,event_timestamps,"
+    "src_expected,dst_expected",
     [
-        (12, 5, 4, 10),
-        (24, 12, 10, 15),
+        (
+            5,
+            torch.tensor(
+                [
+                    EVENT_TYPE_ID_MAP["start"],
+                    EVENT_TYPE_ID_MAP["node-add"],
+                    EVENT_TYPE_ID_MAP["end"],
+                ]
+            ),
+            torch.tensor([1, 1, 3]),
+            torch.tensor([1, 1, 2]),
+            torch.tensor([[1] * 4, [2] * 4, [3] * 4]),
+            torch.tensor([1, 2, 3]),
+            torch.tensor(
+                [
+                    [0] * 4 + [0] * 4 + [0] * 4 + [0] * 4 + [0] * 4,
+                    [3] * 4 + [0] * 4 + [0] * 4 + [5] * 4 + [2] * 4,
+                    [0] * 4 + [0] * 4 + [0] * 4 + [0] * 4 + [0] * 4,
+                ]
+            ).float(),
+            torch.zeros(3, 20),
+        ),
+        (
+            10,
+            torch.tensor(
+                [
+                    EVENT_TYPE_ID_MAP["start"],
+                    EVENT_TYPE_ID_MAP["node-add"],
+                    EVENT_TYPE_ID_MAP["node-delete"],
+                    EVENT_TYPE_ID_MAP["edge-add"],
+                    EVENT_TYPE_ID_MAP["edge-delete"],
+                    EVENT_TYPE_ID_MAP["end"],
+                    EVENT_TYPE_ID_MAP["pad"],
+                    EVENT_TYPE_ID_MAP["pad"],
+                ]
+            ),
+            torch.tensor([0, 0, 1, 2, 3, 0, 0, 0]),
+            torch.tensor([0, 0, 0, 2, 1, 0, 0, 0]),
+            torch.tensor([[i + 1] * 4 for i in range(8)]),
+            torch.linspace(1, 9, 8).long(),
+            torch.tensor(
+                [
+                    [0] * 4 + [0] * 4 + [0] * 4 + [0] * 4 + [0] * 4,
+                    [3] * 4 + [0] * 4 + [0] * 4 + [5] * 4 + [2] * 4,
+                    [4] * 4 + [1] * 4 + [0] * 4 + [6] * 4 + [3] * 4,
+                    [5] * 4 + [2] * 4 + [2] * 4 + [3] * 4 + [4] * 4,
+                    [6] * 4 + [3] * 4 + [1] * 4 + [2] * 4 + [5] * 4,
+                    [0] * 4 + [0] * 4 + [0] * 4 + [0] * 4 + [0] * 4,
+                    [0] * 4 + [0] * 4 + [0] * 4 + [0] * 4 + [0] * 4,
+                    [0] * 4 + [0] * 4 + [0] * 4 + [0] * 4 + [0] * 4,
+                ]
+            ).float(),
+            torch.tensor(
+                [
+                    [0] * 4 + [0] * 4 + [0] * 4 + [0] * 4 + [0] * 4,
+                    [0] * 4 + [0] * 4 + [0] * 4 + [0] * 4 + [0] * 4,
+                    [0] * 4 + [0] * 4 + [0] * 4 + [0] * 4 + [0] * 4,
+                    [5] * 4 + [2] * 4 + [2] * 4 + [3] * 4 + [4] * 4,
+                    [6] * 4 + [1] * 4 + [3] * 4 + [6] * 4 + [5] * 4,
+                    [0] * 4 + [0] * 4 + [0] * 4 + [0] * 4 + [0] * 4,
+                    [0] * 4 + [0] * 4 + [0] * 4 + [0] * 4 + [0] * 4,
+                    [0] * 4 + [0] * 4 + [0] * 4 + [0] * 4 + [0] * 4,
+                ]
+            ).float(),
+        ),
     ],
 )
-def test_tgn_message(hidden_dim, event_seq_len, num_nodes, max_timestamp):
-    tgn = TemporalGraphNetwork(hidden_dim)
+def test_tgn_message(
+    num_nodes,
+    event_type_ids,
+    src_ids,
+    dst_ids,
+    event_embeddings,
+    event_timestamps,
+    src_expected,
+    dst_expected,
+):
+    tgn = TemporalGraphNetwork(4)
     for i in range(num_nodes):
-        tgn.memory[i] = torch.rand(hidden_dim)
-        tgn.last_update[i] = random.randint(0, max_timestamp)
+        tgn.memory[i] = torch.tensor([i] * 4).float()
+        tgn.last_update[i] = i * 2
+
+    class MockTimeEncoder(nn.Module):
+        def forward(self, timestamp):
+            return timestamp.unsqueeze(-1).expand(-1, 4) + 3
+
+    tgn.time_encoder = MockTimeEncoder()
+
+    event_mask, src_mask, dst_mask = compute_masks_from_event_type_ids(event_type_ids)
     src_messages, dst_messages = tgn.message(
-        torch.randint(len(EVENT_TYPES), (event_seq_len,)),
-        torch.randint(num_nodes, (event_seq_len,)),
-        torch.randint(2, (event_seq_len,)).float(),
-        torch.randint(num_nodes, (event_seq_len,)),
-        torch.randint(2, (event_seq_len,)).float(),
-        torch.rand(event_seq_len, hidden_dim),
-        torch.randint(5, (event_seq_len,)),
+        event_type_ids,
+        src_ids,
+        src_mask,
+        dst_ids,
+        dst_mask,
+        event_embeddings,
+        event_mask,
+        event_timestamps,
     )
-    assert src_messages.size() == (event_seq_len, 5 * hidden_dim)
-    assert dst_messages.size() == (event_seq_len, 5 * hidden_dim)
+    assert src_messages.equal(src_expected)
+    assert dst_messages.equal(dst_expected)
 
 
 @pytest.mark.parametrize(