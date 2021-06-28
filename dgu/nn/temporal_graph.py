Fix relative timestamp calculation in TGN message calculation

dgu/nn/temporal_graph.py
@@ -20,10 +20,10 @@ class TemporalGraphNetwork(nn.Module):
             "memory", torch.zeros(max_num_nodes, hidden_dim), persistent=False
         )
 
-        # last updated timestamp, not persistent as we shouldn't save last updated
-        # timestamps from one game to another
+        # last updated timestamp for edges, not persistent as we shouldn't save last
+        # updated timestamps from one game to another
         self.register_buffer(
-            "last_update", torch.zeros(max_num_nodes), persistent=False
+            "last_update", torch.zeros(max_num_edges), persistent=False
         )
 
         # node features, not persistent as we shouldn't save node features
@@ -50,6 +50,7 @@ class TemporalGraphNetwork(nn.Module):
         dst_mask: torch.Tensor,
         event_embeddings: torch.Tensor,
         event_mask: torch.Tensor,
+        event_edge_ids: torch.Tensor,
         event_timestamps: torch.Tensor,
     ) -> Tuple[torch.Tensor, torch.Tensor]:
         """
@@ -66,6 +67,7 @@ class TemporalGraphNetwork(nn.Module):
         dst_mask: (event_seq_len)
         event_embeddings: (event_seq_len, hidden_dim)
         event_mask: (event_seq_len)
+        event_edge_ids: (event_seq_len)
         event_timestamps: (event_seq_len)
 
         output:
@@ -82,24 +84,27 @@ class TemporalGraphNetwork(nn.Module):
         dst_embs = self.memory[dst_ids] * dst_mask.unsqueeze(-1)  # type: ignore
         # (event_seq_len, hidden_dim)
 
-        # fetch last update timestamps
-        src_last_update = self.last_update[src_ids]  # type: ignore
-        # (event_seq_len)
-        dst_last_update = self.last_update[dst_ids]  # type: ignore
+        # calculate relative timestamps for edge events
+        edge_last_update = self.last_update[event_edge_ids]  # type: ignore
         # (event_seq_len)
 
-        # multiply src_last_update and dst_last_update by dst_mask so that we
+        # multiply edge_last_update by dst_mask so that we
         # only subtract last update timestamps for edge events
-        # then pass them through the time encoder to get the embeddings.
-        # after that we mask out special events that don't require timestamps
-        src_timestamp_emb = self.time_encoder(
-            event_timestamps - src_last_update * dst_mask
+        rel_edge_timestamps = event_timestamps - edge_last_update * dst_mask
+        # (event_seq_len)
+
+        # only select node events from event_timestamps
+        # and only select edge events from rel_edge_timestamps (use dst_mask)
+        # then add them and pass it through the time encoder to get the embeddings
+        # finally, mask out timestamp embeddings for special events (use event_mask).
+        is_node_event = torch.logical_or(
+            event_type_ids == EVENT_TYPE_ID_MAP["node-add"],
+            event_type_ids == EVENT_TYPE_ID_MAP["node-delete"],
         )
-        # (event_seq_len, hidden_dim)
-        # mask out node events
-        dst_timestamp_emb = self.time_encoder(
-            event_timestamps - dst_last_update * dst_mask
-        )
+        # (event_seq_len)
+        timestamp_emb = self.time_encoder(
+            event_timestamps * is_node_event + rel_edge_timestamps * dst_mask
+        ) * event_mask.unsqueeze(-1)
         # (event_seq_len, hidden_dim)
 
         # mask out special events
@@ -109,7 +114,7 @@ class TemporalGraphNetwork(nn.Module):
                     event_type_embs,
                     src_embs,
                     dst_embs,
-                    src_timestamp_emb,
+                    timestamp_emb,
                     event_embeddings,
                 ],
                 dim=1,
@@ -123,7 +128,7 @@ class TemporalGraphNetwork(nn.Module):
                     event_type_embs,
                     dst_embs,
                     src_embs,
-                    dst_timestamp_emb,
+                    timestamp_emb,
                     event_embeddings,
                 ],
                 dim=1,

tests/nn/test_temporal_graph.py
@@ -8,11 +8,12 @@ from dgu.constants import EVENT_TYPE_ID_MAP
 
 
 @pytest.mark.parametrize(
-    "num_nodes,event_type_ids,src_ids,dst_ids,event_embeddings,event_timestamps,"
-    "src_expected,dst_expected",
+    "num_nodes,num_edges,event_type_ids,src_ids,dst_ids,event_embeddings,"
+    "event_edge_ids,event_timestamps,src_expected,dst_expected",
     [
         (
             5,
+            7,
             torch.tensor(
                 [
                     EVENT_TYPE_ID_MAP["start"],
@@ -23,6 +24,7 @@ from dgu.constants import EVENT_TYPE_ID_MAP
             torch.tensor([1, 1, 3]),
             torch.tensor([1, 1, 2]),
             torch.tensor([[1] * 4, [2] * 4, [3] * 4]),
+            torch.tensor([0, 0, 0]),
             torch.tensor([1, 2, 3]),
             torch.tensor(
                 [
@@ -34,7 +36,38 @@ from dgu.constants import EVENT_TYPE_ID_MAP
             torch.zeros(3, 20),
         ),
         (
+            5,
+            7,
+            torch.tensor(
+                [
+                    EVENT_TYPE_ID_MAP["start"],
+                    EVENT_TYPE_ID_MAP["edge-add"],
+                    EVENT_TYPE_ID_MAP["end"],
+                ]
+            ),
+            torch.tensor([1, 1, 3]),
+            torch.tensor([1, 2, 2]),
+            torch.tensor([[1] * 4, [2] * 4, [3] * 4]),
+            torch.tensor([0, 0, 0]),
+            torch.tensor([1, 2, 3]),
+            torch.tensor(
+                [
+                    [0] * 4 + [0] * 4 + [0] * 4 + [0] * 4 + [0] * 4,
+                    [5] * 4 + [1] * 4 + [2] * 4 + [5] * 4 + [2] * 4,
+                    [0] * 4 + [0] * 4 + [0] * 4 + [0] * 4 + [0] * 4,
+                ]
+            ).float(),
+            torch.tensor(
+                [
+                    [0] * 4 + [0] * 4 + [0] * 4 + [0] * 4 + [0] * 4,
+                    [5] * 4 + [2] * 4 + [1] * 4 + [5] * 4 + [2] * 4,
+                    [0] * 4 + [0] * 4 + [0] * 4 + [0] * 4 + [0] * 4,
+                ]
+            ).float(),
+        ),
+        (
             10,
+            20,
             torch.tensor(
                 [
                     EVENT_TYPE_ID_MAP["start"],
@@ -48,16 +81,17 @@ from dgu.constants import EVENT_TYPE_ID_MAP
                 ]
             ),
             torch.tensor([0, 0, 1, 2, 3, 0, 0, 0]),
-            torch.tensor([0, 0, 0, 2, 1, 0, 0, 0]),
+            torch.tensor([0, 0, 0, 3, 1, 0, 0, 0]),
             torch.tensor([[i + 1] * 4 for i in range(8)]),
-            torch.linspace(1, 9, 8).long(),
+            torch.tensor([0, 0, 0, 2, 4, 0, 0, 0]),
+            torch.linspace(1, 8, 8).long(),
             torch.tensor(
                 [
                     [0] * 4 + [0] * 4 + [0] * 4 + [0] * 4 + [0] * 4,
                     [3] * 4 + [0] * 4 + [0] * 4 + [5] * 4 + [2] * 4,
                     [4] * 4 + [1] * 4 + [0] * 4 + [6] * 4 + [3] * 4,
-                    [5] * 4 + [2] * 4 + [2] * 4 + [3] * 4 + [4] * 4,
-                    [6] * 4 + [3] * 4 + [1] * 4 + [2] * 4 + [5] * 4,
+                    [5] * 4 + [2] * 4 + [3] * 4 + [3] * 4 + [4] * 4,
+                    [6] * 4 + [3] * 4 + [1] * 4 + [0] * 4 + [5] * 4,
                     [0] * 4 + [0] * 4 + [0] * 4 + [0] * 4 + [0] * 4,
                     [0] * 4 + [0] * 4 + [0] * 4 + [0] * 4 + [0] * 4,
                     [0] * 4 + [0] * 4 + [0] * 4 + [0] * 4 + [0] * 4,
@@ -68,8 +102,8 @@ from dgu.constants import EVENT_TYPE_ID_MAP
                     [0] * 4 + [0] * 4 + [0] * 4 + [0] * 4 + [0] * 4,
                     [0] * 4 + [0] * 4 + [0] * 4 + [0] * 4 + [0] * 4,
                     [0] * 4 + [0] * 4 + [0] * 4 + [0] * 4 + [0] * 4,
-                    [5] * 4 + [2] * 4 + [2] * 4 + [3] * 4 + [4] * 4,
-                    [6] * 4 + [1] * 4 + [3] * 4 + [6] * 4 + [5] * 4,
+                    [5] * 4 + [3] * 4 + [2] * 4 + [3] * 4 + [4] * 4,
+                    [6] * 4 + [1] * 4 + [3] * 4 + [0] * 4 + [5] * 4,
                     [0] * 4 + [0] * 4 + [0] * 4 + [0] * 4 + [0] * 4,
                     [0] * 4 + [0] * 4 + [0] * 4 + [0] * 4 + [0] * 4,
                     [0] * 4 + [0] * 4 + [0] * 4 + [0] * 4 + [0] * 4,
@@ -80,17 +114,20 @@ from dgu.constants import EVENT_TYPE_ID_MAP
 )
 def test_tgn_message(
     num_nodes,
+    num_edges,
     event_type_ids,
     src_ids,
     dst_ids,
     event_embeddings,
+    event_edge_ids,
     event_timestamps,
     src_expected,
     dst_expected,
 ):
-    tgn = TemporalGraphNetwork(num_nodes, 5, 4)
+    tgn = TemporalGraphNetwork(num_nodes, num_edges, 4)
     for i in range(num_nodes):
         tgn.memory[i] = torch.tensor([i] * 4).float()
+    for i in range(num_edges):
         tgn.last_update[i] = i * 2
 
     class MockTimeEncoder(nn.Module):
@@ -108,6 +145,7 @@ def test_tgn_message(
         dst_mask,
         event_embeddings,
         event_mask,
+        event_edge_ids,
         event_timestamps,
     )
     assert src_messages.equal(src_expected)