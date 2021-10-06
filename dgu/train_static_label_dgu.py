693b447bfcac8c56e4f4debd00858d5170698b79
Peter Yu <2057325+yukw777@users.noreply.github.com>
2021-10-06 15:32:39 -0400

    Turn checkpoint path to absolute path when test_only=true

--- dgu/train_static_label_dgu.py
+++ dgu/train_static_label_dgu.py
@@ -2,7 +2,7 @@ import hydra
 import pytorch_lightning as pl
 
 from omegaconf import DictConfig, OmegaConf
-from hydra.utils import instantiate, get_class
+from hydra.utils import instantiate, get_class, to_absolute_path
 from pytorch_lightning.callbacks import ModelCheckpoint
 
 
@@ -36,7 +36,7 @@ def main(cfg: DictConfig) -> None:
     if cfg.test_only:
         model_class = get_class(cfg.model._target_)
         lm = model_class.load_from_checkpoint(  # type: ignore
-            cfg.trainer.resume_from_checkpoint,
+            to_absolute_path(cfg.trainer.resume_from_checkpoint),
             word_vocab_path=cfg.data_module.word_vocab_path,
             node_vocab_path=cfg.data_module.node_vocab_path,
             relation_vocab_path=cfg.data_module.relation_vocab_path,
