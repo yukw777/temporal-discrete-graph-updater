f3f96347d42ce66074a14aceba99fb50587a8ce8
Peter Yu <2057325+yukw777@users.noreply.github.com>
2021-11-06 21:03:13 -0400

    Use test_batch_size for test_dataloader

--- dgu/data.py
+++ dgu/data.py
@@ -592,7 +592,7 @@ class TWCmdGenTemporalDataModule(pl.LightningDataModule):
     def test_dataloader(self) -> DataLoader:
         return DataLoader(
             self.test,
-            batch_size=self.val_batch_size,
+            batch_size=self.test_batch_size,
             collate_fn=TWCmdGenTemporalDataCollator(
                 self.preprocessor, self.label_id_map
             ),
