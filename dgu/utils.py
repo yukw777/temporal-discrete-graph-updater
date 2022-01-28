e695e35e2df5aab2bf5df56589335ee23a50ca3e
Peter Yu <2057325+yukw777@users.noreply.github.com>
2022-01-28 00:13:22 -0500

    Add layout_engine option to draw_graph

--- dgu/utils.py
+++ dgu/utils.py
@@ -1,7 +1,7 @@
 import networkx as nx
 
 
-def draw_graph(graph: nx.DiGraph, filename: str) -> None:
+def draw_graph(graph: nx.DiGraph, filename: str, layout_engine="sfdp") -> None:
     agraph = nx.nx_agraph.to_agraph(graph)
     agraph.node_attr.update(
         fontsize="20pt", fontname="helvetica-bold", shape="plaintext"
@@ -9,5 +9,5 @@ def draw_graph(graph: nx.DiGraph, filename: str) -> None:
     agraph.edge_attr.update(
         fontsize="20pt", fontname="helvetica", color="gray", fontcolor="dodgerblue"
     )
-    agraph.layout(prog="sfdp")
+    agraph.layout(prog=layout_engine)
     agraph.draw(filename)
