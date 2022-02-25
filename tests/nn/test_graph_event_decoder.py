e197c196d2483f6d864f10e41eff2185478887f8
Peter Yu <2057325+yukw777@users.noreply.github.com>
2022-02-25 09:25:23 -0500

    Implement EventSequentialLabelHead
    EventSequentialLabelHead is an autoregressive decoder that can generate
    labels.

--- tests/nn/test_graph_event_decoder.py
+++ tests/nn/test_graph_event_decoder.py
@@ -1,15 +1,19 @@
 import pytest
 import torch
+import torch.nn as nn
+import random
 
 from tdgu.nn.graph_event_decoder import (
     EventTypeHead,
     EventNodeHead,
     EventStaticLabelHead,
+    EventSequentialLabelHead,
     RNNGraphEventDecoder,
     TransformerGraphEventDecoderBlock,
     TransformerGraphEventDecoder,
 )
 from tdgu.constants import EVENT_TYPES
+from tdgu.preprocessor import BOS, EOS, PAD
 
 
 @pytest.mark.parametrize("dropout", [0.0, 0.3, 0.5])
@@ -128,6 +132,194 @@ def test_event_static_label_head(
         )
     label_logits = head(torch.rand(batch, autoregressive_embedding_dim))
     assert label_logits.size() == (batch, num_label)
+
+
+@pytest.fixture
+def word_to_id_dict():
+    return {
+        PAD: 0,
+        BOS: 1,
+        EOS: 2,
+        "word0": 3,
+        "word1": 4,
+        "word2": 5,
+    }
+
+
+@pytest.mark.parametrize("seq_len", [1, 5, 10])
+@pytest.mark.parametrize(
+    "autoregressive_embedding_dim,hidden_dim,word_embedding_dim,batch",
+    [(8, 4, 16, 1), (8, 4, 16, 4)],
+)
+def test_event_seq_label_head_forward(
+    word_to_id_dict,
+    autoregressive_embedding_dim,
+    hidden_dim,
+    word_embedding_dim,
+    batch,
+    seq_len,
+):
+    head = EventSequentialLabelHead(
+        autoregressive_embedding_dim,
+        hidden_dim,
+        word_to_id_dict,
+        nn.Embedding(
+            len(word_to_id_dict),
+            word_embedding_dim,
+            padding_idx=word_to_id_dict["<pad>"],
+        ),
+    )
+
+    output_tgt_seq = torch.randint(len(word_to_id_dict), (batch, seq_len))
+    seq_mask_list = [[True] * seq_len]
+    for _ in range(batch - 1):
+        length = random.randrange(1, seq_len) if seq_len > 1 else 1
+        seq_mask_list.append([True] * length + [False] * (seq_len - length))
+    output_tgt_seq_mask = torch.tensor(seq_mask_list)
+    autoregressive_embedding = torch.rand(batch, autoregressive_embedding_dim)
+    prev_hidden = torch.rand(batch, hidden_dim)
+    # either autoregressive_embedding or prev_hidden has to be provided
+    with pytest.raises(ValueError):
+        head(output_tgt_seq, output_tgt_seq_mask)
+
+    # only one of autoregressive_embedding or prev_hidden has to be provided
+    with pytest.raises(AssertionError):
+        head(
+            output_tgt_seq,
+            output_tgt_seq_mask,
+            autoregressive_embedding=autoregressive_embedding,
+            prev_hidden=prev_hidden,
+        )
+
+    # forward pass with autoregressive_embedding
+    output_seq_logits, updated_hidden = head(
+        output_tgt_seq,
+        output_tgt_seq_mask,
+        autoregressive_embedding=autoregressive_embedding,
+    )
+    assert output_seq_logits.size() == (batch, seq_len, len(word_to_id_dict))
+    assert updated_hidden.size() == (batch, hidden_dim)
+
+    # forward pass with prev_hidden
+    output_seq_logits, updated_hidden = head(
+        output_tgt_seq, output_tgt_seq_mask, prev_hidden=prev_hidden
+    )
+    assert output_seq_logits.size() == (batch, seq_len, len(word_to_id_dict))
+    assert updated_hidden.size() == (batch, hidden_dim)
+
+
+# @pytest.mark.parametrize("max_decode_len", [None, 5, 15])
+@pytest.mark.parametrize("autoregressive_embedding_dim", [4, 16])
+@pytest.mark.parametrize(
+    "batch,forward_logits,max_decode_len,expected_decoded,expected_mask",
+    [
+        (
+            1,
+            [torch.tensor([[0, 0, 1, 0, 0, 0]]).float().unsqueeze(1)],
+            None,
+            torch.tensor([[2]]),
+            torch.tensor([[True]]),
+        ),
+        (
+            1,
+            [
+                torch.tensor([[0, 0, 0, 1, 0, 0]]).float().unsqueeze(1),
+                torch.tensor([[0, 0, 0, 0, 1, 0]]).float().unsqueeze(1),
+                torch.tensor([[0, 0, 1, 0, 0, 0]]).float().unsqueeze(1),
+            ],
+            None,
+            torch.tensor([[3, 4, 2]]),
+            torch.ones(1, 3).bool(),
+        ),
+        (
+            1,
+            [
+                torch.tensor([[0, 0, 0, 1, 0, 0]]).float().unsqueeze(1),
+                torch.tensor([[0, 0, 0, 0, 1, 0]]).float().unsqueeze(1),
+                torch.tensor([[0, 0, 1, 0, 0, 0]]).float().unsqueeze(1),
+            ],
+            2,
+            torch.tensor([[3, 4]]),
+            torch.ones(1, 2).bool(),
+        ),
+        (
+            2,
+            [
+                torch.tensor([[0, 0, 0, 1, 0, 0], [0, 0, 0, 0, 1, 0]])
+                .float()
+                .unsqueeze(1),
+                torch.tensor([[0, 0, 0, 0, 1, 0], [0, 0, 1, 0, 0, 0]])
+                .float()
+                .unsqueeze(1),
+                torch.tensor([[0, 0, 1, 0, 0, 0], [0, 0, 0, 0, 0, 1]])
+                .float()
+                .unsqueeze(1),
+            ],
+            None,
+            torch.tensor([[3, 4, 2], [4, 2, 0]]),
+            torch.tensor([[True, True, True], [True, True, False]]),
+        ),
+        (
+            2,
+            [
+                torch.tensor([[0, 0, 0, 1, 0, 0], [0, 0, 0, 0, 1, 0]])
+                .float()
+                .unsqueeze(1),
+                torch.tensor([[0, 0, 0, 0, 1, 0], [0, 0, 1, 0, 0, 0]])
+                .float()
+                .unsqueeze(1),
+                torch.tensor([[0, 0, 1, 0, 0, 0], [0, 0, 0, 0, 0, 1]])
+                .float()
+                .unsqueeze(1),
+            ],
+            1,
+            torch.tensor([[3], [4]]),
+            torch.ones(2, 1).bool(),
+        ),
+    ],
+)
+def test_event_seq_label_head_greedy_decode(
+    word_to_id_dict,
+    batch,
+    forward_logits,
+    max_decode_len,
+    expected_decoded,
+    expected_mask,
+    autoregressive_embedding_dim,
+):
+    head = EventSequentialLabelHead(
+        autoregressive_embedding_dim,
+        4,
+        word_to_id_dict,
+        nn.Embedding(
+            len(word_to_id_dict),
+            12,
+            padding_idx=word_to_id_dict["<pad>"],
+        ),
+    )
+
+    class MockForward:
+        def __init__(self):
+            self.num_calls = 0
+
+        def __call__(self, *args, **kwargs):
+            logits = forward_logits[self.num_calls]
+            self.num_calls += 1
+            return logits, torch.rand(batch, 4)
+
+    head.forward = MockForward()
+
+    if max_decode_len is None:
+        decoded, mask = head.greedy_decode(
+            torch.rand(batch, autoregressive_embedding_dim)
+        )
+    else:
+        decoded, mask = head.greedy_decode(
+            torch.rand(batch, autoregressive_embedding_dim),
+            max_decode_len=max_decode_len,
+        )
+    assert decoded.equal(expected_decoded)
+    assert mask.equal(expected_mask)
 
 
 @pytest.mark.parametrize(
