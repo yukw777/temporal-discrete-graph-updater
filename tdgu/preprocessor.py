8486717f8c3553377db2cea2e9043af18369a65c
M-XI <ximic@umich.edu>
2022-03-15 16:12:18 -0400

    Added the huggingface tokenizer to preprocessor.py along with assoicated test cases

--- tdgu/preprocessor.py
+++ tdgu/preprocessor.py
@@ -3,6 +3,8 @@ import torch
 from typing import List, Tuple, Optional
 from spacy.lang.en import English
 
+from transformers import DistilBertTokenizer
+
 PAD = "<pad>"
 UNK = "<unk>"
 BOS = "<bos>"
@@ -10,9 +12,76 @@ EOS = "<eos>"
 SEP = "<sep>"
 
 
-class SpacyPreprocessor:
+class Preprocessor:
     def __init__(self, word_vocab: List[str]) -> None:
-        super().__init__()
+        pass
+
+    def id_to_word(self, word_id: int) -> str:
+        pass
+
+    def ids_to_words(self, word_ids: List[int]) -> List[str]:
+        pass
+
+    def word_to_id(self, word: str) -> int:
+        pass
+
+    def words_to_ids(self, words: List[str]) -> List[int]:
+        pass
+
+    def tokenize(self, s: str) -> List[str]:
+        pass
+
+    def pad(
+        self, unpadded_batch: List[List[int]], device: Optional[torch.device] = None
+    ) -> Tuple[torch.Tensor, torch.Tensor]:
+        pass
+
+    def preprocess_tokenized(
+        self, tokenized_batch: List[List[str]], device: Optional[torch.device] = None
+    ) -> Tuple[torch.Tensor, torch.Tensor]:
+        pass
+
+    def preprocess(
+        self, batch: List[str], device: Optional[torch.device] = None
+    ) -> Tuple[torch.Tensor, torch.Tensor]:
+        pass
+
+    def clean_and_preprocess(
+        self, batch: List[str], device: Optional[torch.device] = None
+    ) -> Tuple[torch.Tensor, torch.Tensor]:
+        pass
+
+    def batch_clean(self, batch_raw_str: List[str]) -> List[str]:
+        pass
+
+    def clean(self, raw_str: Optional[str]) -> str:
+        """
+        Copied from the original GATA code (preproc())
+        """
+        if raw_str is None:
+            return "nothing"
+        cleaned = raw_str.replace("\n", " ")
+        if "$$$$$$$" in cleaned:
+            cleaned = cleaned.split("$$$$$$$")[-1]
+        while "  " in cleaned:
+            cleaned = cleaned.replace("  ", " ")
+        cleaned = cleaned.strip()
+        if len(cleaned) == 0:
+            return "nothing"
+        return cleaned
+
+    def decode(self, batch: List[List[int]]) -> List[str]:
+        pass
+
+    @classmethod
+    def load_from_file(cls, word_vocab_path: str) -> "Preprocessor":
+        pass
+
+
+class SpacyPreprocessor(Preprocessor):
+    def __init__(self, word_vocab: List[str]) -> None:
+        super().__init__(word_vocab)
+
         self.tokenizer = English().tokenizer
         self.word_vocab = word_vocab
         self.word_to_id_dict = {w: i for i, w in enumerate(word_vocab)}
@@ -83,20 +152,7 @@ class SpacyPreprocessor:
         return [self.clean(raw_str) for raw_str in batch_raw_str]
 
     def clean(self, raw_str: Optional[str]) -> str:
-        """
-        Copied from the original GATA code (preproc())
-        """
-        if raw_str is None:
-            return "nothing"
-        cleaned = raw_str.replace("\n", " ")
-        if "$$$$$$$" in cleaned:
-            cleaned = cleaned.split("$$$$$$$")[-1]
-        while "  " in cleaned:
-            cleaned = cleaned.replace("  ", " ")
-        cleaned = cleaned.strip()
-        if len(cleaned) == 0:
-            return "nothing"
-        return cleaned
+        return super().clean(raw_str)
 
     def decode(self, batch: List[List[int]]) -> List[str]:
         return [
@@ -113,3 +169,120 @@ class SpacyPreprocessor:
         with open(word_vocab_path, "r") as f:
             word_vocab = [word.strip() for word in f]
         return cls(word_vocab)
+
+
+class BERTPreprocessor(Preprocessor):
+    def __init__(self, word_vocab: List[str] = []) -> None:
+        super().__init__(word_vocab)
+        self.tokenizer = DistilBertTokenizer.from_pretrained(
+            "distilbert-base-uncased", use_fast=True
+        )
+
+        # NOTE: Acceptable to have different tokens from Spacy
+        # special_token_dict = {
+        #     "pad_token" : PAD,
+        #     "unk_token" : UNK,
+        #     "bos_token" : BOS,
+        #     "eos_token" : EOS,
+        #     "sep_token" : SEP
+        # }
+        # self.tokenizer.add_special_tokens(special_token_dict)
+        # self.pad_id = self.word_to_id_dict[PAD]
+        # self.unk_id = self.word_to_id_dict[UNK]
+
+        DistilBERT_vocab = self.tokenizer.get_vocab()
+        self.word_vocab = list(DistilBERT_vocab.keys())
+        self.word_to_id_dict = DistilBERT_vocab
+
+    # NOTE: The BERT pretrained tokenizer is rather specific about
+    # tokenization before mapping to ids
+    def id_to_word(self, word_id: int) -> str:
+        return self.tokenizer.convert_ids_to_tokens(word_id)
+
+    def ids_to_words(self, word_ids: List[int]) -> List[str]:
+        return self.tokenizer.convert_ids_to_tokens(word_ids)
+
+    def word_to_id(self, word: str) -> int:
+        return self.tokenizer.convert_tokens_to_ids(word)
+
+    def words_to_ids(self, words: List[str]) -> List[int]:
+        return self.tokenizer.convert_tokens_to_ids(words)
+
+    def tokenize(self, s: str) -> List[str]:
+        return self.tokenizer.tokenize(s)
+
+    # NOTE: Default padding to max model length to maintain consistency
+    # with tokenizer __call__ (in this case 512)
+    def pad(
+        self, unpadded_batch: List[List[int]], device: Optional[torch.device] = None
+    ) -> Tuple[torch.Tensor, torch.Tensor]:
+        # return padded tensor and corresponding mask
+        max_len = self.tokenizer.max_model_input_sizes["distilbert-base-uncased"]
+        max_len = min(max_len, max(len(word_ids) for word_ids in unpadded_batch))
+        return (
+            torch.tensor(
+                [
+                    word_ids + [0] * (max_len - len(word_ids))
+                    for word_ids in unpadded_batch
+                ],
+                device=device,
+            ),
+            torch.tensor(
+                [
+                    [1] * len(word_ids) + [0] * (max_len - len(word_ids))
+                    for word_ids in unpadded_batch
+                ],
+                device=device,
+                dtype=torch.bool,
+            ),
+        )
+
+    # NOTE: Assumes the input is already correctly formatted with CLS, etc. tokens
+    # NOTE: Mask is in the form of bool(s) rather than (long) int(s) or float(s),
+    # this distinction is kept to maintain consistency in data.py
+    def preprocess_tokenized(
+        self, tokenized_batch: List[List[str]], device: Optional[torch.device] = None
+    ) -> Tuple[torch.Tensor, torch.Tensor]:
+        return self.pad(
+            [self.words_to_ids(tokenized) for tokenized in tokenized_batch],
+            device=device,
+        )
+
+    # NOTE: Assumes the input has no formatting at all (will add CLS, etc. tokens)
+    # NOTE: Mask is in the form of (long) int(s) rather than bool(s),
+    # this distinction is made so the transformer may accept it
+    def preprocess(
+        self, batch: List[str], device: Optional[torch.device] = None
+    ) -> Tuple[torch.Tensor, torch.Tensor]:
+        processed = self.tokenizer(
+            batch, return_tensors="pt", padding="longest", truncation="longest_first"
+        )
+        return (
+            processed["input_ids"].to(device=device),
+            processed["attention_mask"].to(device=device),
+        )
+
+    # NOTE: preprocess_tokenized preceded by clean
+    def clean_and_preprocess(
+        self, batch: List[str], device: Optional[torch.device] = None
+    ) -> Tuple[torch.Tensor, torch.Tensor]:
+        return self.preprocess_tokenized(
+            [self.tokenize(self.clean(s)) for s in batch], device=device
+        )
+
+    def batch_clean(self, batch_raw_str: List[str]) -> List[str]:
+        return [self.clean(raw_str) for raw_str in batch_raw_str]
+
+    def clean(self, raw_str: Optional[str]) -> str:
+        return super().clean(raw_str)
+
+    def decode(self, batch: List[List[int]]) -> List[str]:
+        return [
+            self.tokenizer.decode(word_ids, skip_special_tokens=True)
+            for word_ids in batch
+        ]
+
+    # NOTE: No need to load from file, simply downloads the vocabulary
+    @classmethod
+    def load_from_file(cls, word_vocab_path: str) -> "BERTPreprocessor":
+        return cls()
--- tests/test_preprocessor.py
+++ tests/test_preprocessor.py
@@ -1,7 +1,7 @@
 import pytest
 import torch
 
-from tdgu.preprocessor import SpacyPreprocessor
+from tdgu.preprocessor import BERTPreprocessor, SpacyPreprocessor
 
 
 @pytest.mark.parametrize(
@@ -36,6 +36,38 @@ def test_spacy_preprocessor_preprocess(batch, expected_preprocessed, expected_ma
     [
         (
             ["My name is Peter"],
+            torch.tensor([[101, 2026, 2171, 2003, 2848, 102]]),
+            torch.tensor([[1, 1, 1, 1, 1, 1]]),
+        ),
+        (
+            ["my name is peter"],
+            torch.tensor([[101, 2026, 2171, 2003, 2848, 102]]),
+            torch.tensor([[1, 1, 1, 1, 1, 1]]),
+        ),
+        (
+            ["My name is Peter", "Is my name David?"],
+            torch.tensor(
+                [
+                    [101, 2026, 2171, 2003, 2848, 102, 0],
+                    [101, 2003, 2026, 2171, 2585, 1029, 102],
+                ]
+            ),
+            torch.tensor([[1, 1, 1, 1, 1, 1, 0], [1, 1, 1, 1, 1, 1, 1]]),
+        ),
+    ],
+)
+def test_BERT_preprocessor_preprocess(batch, expected_preprocessed, expected_mask):
+    sp = BERTPreprocessor(["<pad>", "<unk>", "my", "name", "is", "peter"])
+    preprocessed, mask = sp.preprocess(batch)
+    assert preprocessed.equal(expected_preprocessed)
+    assert mask.equal(expected_mask)
+
+
+@pytest.mark.parametrize(
+    "batch,expected_preprocessed,expected_mask",
+    [
+        (
+            ["My name is Peter"],
             torch.tensor([[2, 3, 4, 5]]),
             torch.tensor([[1, 1, 1, 1]]).float(),
         ),
@@ -59,9 +91,42 @@ def test_spacy_preprocessor_decode(batch, expected_preprocessed, expected_mask):
     assert sp.decode(preprocessed.tolist()) == [" ".join(sp.tokenize(s)) for s in batch]
 
 
+@pytest.mark.parametrize(
+    "batch,expected_preprocessed,expected_mask",
+    [
+        (
+            ["My name is Peter"],
+            torch.tensor([[2026, 2171, 2003, 2848]]),
+            torch.tensor([[1, 1, 1, 1]]),
+        ),
+        (
+            ["my name is peter"],
+            torch.tensor([[2026, 2171, 2003, 2848]]),
+            torch.tensor([[1, 1, 1, 1]]),
+        ),
+        (
+            ["My name is Peter", "Is my name David?"],
+            torch.tensor([[2026, 2171, 2003, 2848, 0], [2003, 2026, 2171, 2585, 1029]]),
+            torch.tensor([[1, 1, 1, 1, 0], [1, 1, 1, 1, 1]]),
+        ),
+    ],
+)
+def test_BERT_preprocessor_decode(batch, expected_preprocessed, expected_mask):
+    sp = BERTPreprocessor(["<pad>", "<unk>", "my", "name", "is", "peter", "david", "?"])
+    preprocessed, _ = sp.preprocess(batch)
+    assert sp.decode(preprocessed.tolist()) == [s.lower() for s in batch]
+
+
 def test_spacy_preprocessor_load_from_file():
     sp = SpacyPreprocessor.load_from_file("vocabs/word_vocab.txt")
     assert len(sp.word_to_id_dict) == 772
+
+
+def test_BERT_preprocessor_load_from_file():
+    sp = BERTPreprocessor.load_from_file("vocabs/word_vocab.txt")
+    sp_alt = BERTPreprocessor()
+    assert len(sp.word_to_id_dict) != 0
+    assert len(sp.word_to_id_dict) == len(sp_alt.word_to_id_dict)
 
 
 @pytest.mark.parametrize("batch_size", list(range(3)))
@@ -130,6 +195,72 @@ def test_spacy_preprocessor_clean(raw_str, cleaned, batch_size):
     assert sp.batch_clean([raw_str] * batch_size) == [cleaned] * batch_size
 
 
+@pytest.mark.parametrize("batch_size", list(range(3)))
+@pytest.mark.parametrize(
+    "raw_str,cleaned",
+    [
+        (None, "nothing"),
+        ("double  spaces!", "double spaces!"),
+        ("many     spaces!", "many spaces!"),
+        ("    ", "nothing"),
+        (
+            "\n\n\n"
+            "                    ________  ________  __    __  ________        \n"
+            "                   |        \\|        \\|  \\  |  \\|        \\       \n"
+            "                    \\$$$$$$$$| $$$$$$$$| $$  | $$ \\$$$$$$$$       \n"
+            "                      | $$   | $$__     \\$$\\/  $$   | $$          \n"
+            "                      | $$   | $$  \\     >$$  $$    | $$          \n"
+            "                      | $$   | $$$$$    /  $$$$\\    | $$          \n"
+            "                      | $$   | $$_____ |  $$ \\$$\\   | $$          \n"
+            "                      | $$   | $$     \\| $$  | $$   | $$          \n"
+            "                       \\$$    \\$$$$$$$$ \\$$   \\$$    \\$$          \n"
+            "              __       __   ______   _______   __        _______  \n"
+            "             |  \\  _  |  \\ /      \\ |       \\ |  \\      |       \\ \n"
+            "             | $$ / \\ | $$|  $$$$$$\\| $$$$$$$\\| $$      | $$$$$$$\\\n"
+            "             | $$/  $\\| $$| $$  | $$| $$__| $$| $$      | $$  | $$\n"
+            "             | $$  $$$\\ $$| $$  | $$| $$    $$| $$      | $$  | $$\n"
+            "             | $$ $$\\$$\\$$| $$  | $$| $$$$$$$\\| $$      | $$  | $$\n"
+            "             | $$$$  \\$$$$| $$__/ $$| $$  | $$| $$_____ | $$__/ $$\n"
+            "             | $$$    \\$$$ \\$$    $$| $$  | $$| $$     \\| $$    $$\n"
+            "              \\$$      \\$$  \\$$$$$$  \\$$   \\$$ \\$$$$$$$$ \\$$$$$$$"
+            " \n\n"
+            "You are hungry! Let's cook a delicious meal. "
+            "Check the cookbook in the kitchen for the recipe. "
+            "Once done, enjoy your meal!\n\n"
+            "-= Kitchen =-\n"
+            "If you're wondering why everything seems so normal all of a sudden, "
+            "it's because you've just shown up in the kitchen.\n\n"
+            "You can see a closed fridge, which looks conventional, "
+            "right there by you. "
+            "You see a closed oven right there by you. Oh, great. Here's a table. "
+            "Unfortunately, there isn't a thing on it. Hm. "
+            "Oh well You scan the room, seeing a counter. The counter is vast. "
+            "On the counter you can make out a cookbook and a knife. "
+            "You make out a stove. Looks like someone's already been here and "
+            "taken everything off it, though. Sometimes, just sometimes, "
+            "TextWorld can just be the worst.\n\n\n",
+            "You are hungry! Let's cook a delicious meal. "
+            "Check the cookbook in the kitchen for the recipe. "
+            "Once done, enjoy your meal! -= Kitchen =- "
+            "If you're wondering why everything seems so normal all of a sudden, "
+            "it's because you've just shown up in the kitchen. "
+            "You can see a closed fridge, which looks conventional, "
+            "right there by you. You see a closed oven right there by you. "
+            "Oh, great. Here's a table. Unfortunately, there isn't a thing on it. "
+            "Hm. Oh well You scan the room, seeing a counter. The counter is vast. "
+            "On the counter you can make out a cookbook and a knife. "
+            "You make out a stove. "
+            "Looks like someone's already been here and taken everything off it, "
+            "though. Sometimes, just sometimes, TextWorld can just be the worst.",
+        ),
+    ],
+)
+def test_BERT_preprocessor_clean(raw_str, cleaned, batch_size):
+    sp = BERTPreprocessor.load_from_file("vocabs/word_vocab.txt")
+    assert sp.clean(raw_str) == cleaned
+    assert sp.batch_clean([raw_str] * batch_size) == [cleaned] * batch_size
+
+
 @pytest.mark.parametrize(
     "batch,expected_preprocessed,expected_mask",
     [
@@ -154,6 +285,35 @@ def test_spacy_preprocessor_clean_preprocess(
     batch, expected_preprocessed, expected_mask
 ):
     sp = SpacyPreprocessor(["<pad>", "<unk>", "my", "name", "is", "peter"])
+    preprocessed, mask = sp.clean_and_preprocess(batch)
+    assert preprocessed.equal(expected_preprocessed)
+    assert mask.equal(expected_mask)
+
+
+@pytest.mark.parametrize(
+    "batch,expected_preprocessed,expected_mask",
+    [
+        (
+            ["$$$$$$$ My name is Peter"],
+            torch.tensor([[2026, 2171, 2003, 2848]]),
+            torch.tensor([[True] * 4]),
+        ),
+        (
+            ["my   name     is  peter"],
+            torch.tensor([[2026, 2171, 2003, 2848]]),
+            torch.tensor([[True] * 4]),
+        ),
+        (
+            ["My    name\n is Peter", "$$$$$$$Is   my name \n\nDavid?"],
+            torch.tensor([[2026, 2171, 2003, 2848, 0], [2003, 2026, 2171, 2585, 1029]]),
+            torch.tensor([[True, True, True, True, False], [True] * 5]),
+        ),
+    ],
+)
+def test_BERT_preprocessor_clean_preprocess(
+    batch, expected_preprocessed, expected_mask
+):
+    sp = BERTPreprocessor(["<pad>", "<unk>", "my", "name", "is", "peter"])
     preprocessed, mask = sp.clean_and_preprocess(batch)
     assert preprocessed.equal(expected_preprocessed)
     assert mask.equal(expected_mask)
