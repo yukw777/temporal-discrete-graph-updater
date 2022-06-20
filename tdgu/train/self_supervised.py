58949b68eb70e9674abe40bbe2505896b52d2620
Peter Yu <2057325+yukw777@users.noreply.github.com>
2022-06-20 13:55:41 -0400

    Use the map-stype dataset for self-supervised, observation generation

--- tdgu/train/self_supervised.py
+++ tdgu/train/self_supervised.py
@@ -3,18 +3,14 @@ import torch.nn as nn
 import pytorch_lightning as pl
 
 from torch.optim import AdamW, Optimizer
-from typing import Dict, Tuple, Any, List, Optional
+from typing import Dict, Tuple, Any, List, Optional, Union
 from torch_geometric.data import Data, Batch
-from hydra.utils import to_absolute_path
-from pathlib import Path
 from pytorch_lightning.trainer.states import RunningStage
 
 from tdgu.train.common import TDGULightningModule
-from tdgu.nn.utils import load_fasttext, shift_tokens_right
-from tdgu.graph import data_list_to_batch, batch_to_data_list
+from tdgu.nn.utils import shift_tokens_right
 from tdgu.nn.text import TextDecoder
-from tdgu.preprocessor import SpacyPreprocessor, PAD, UNK, BOS, EOS
-from tdgu.data import TWCmdGenGraphEventStepInput, TWCmdGenGraphEventDataCollator
+from tdgu.data import TWCmdGenGraphEventStepInput, TWCmdGenObsGenBatch
 from tdgu.metrics import F1
 
 
@@ -26,8 +22,7 @@ class ObsGenSelfSupervisedTDGU(pl.LightningModule):
 
     def __init__(
         self,
-        word_vocab_path: Optional[str] = None,
-        pretrained_word_embedding_path: Optional[str] = None,
+        truncated_bptt_steps: int = 1,
         text_decoder_num_blocks: int = 1,
         text_decoder_num_heads: int = 1,
         **kwargs,
@@ -36,41 +31,9 @@ class ObsGenSelfSupervisedTDGU(pl.LightningModule):
         self.save_hyperparameters(
             ignore=["word_vocab_path", "pretrained_word_embedding_path"]
         )
+        self.truncated_bptt_steps = truncated_bptt_steps
 
-        # preprocessor
-        self.preprocessor = (
-            SpacyPreprocessor([PAD, UNK, BOS, EOS])
-            if word_vocab_path is None
-            else SpacyPreprocessor.load_from_file(to_absolute_path(word_vocab_path))
-        )
-
-        # pretrained word embeddings
-        pretrained_word_embeddings: Optional[nn.Embedding]
-        if pretrained_word_embedding_path is None:
-            pretrained_word_embeddings = None
-        else:
-            abs_pretrained_word_embedding_path = Path(
-                to_absolute_path(pretrained_word_embedding_path)
-            )
-            serialized_path = abs_pretrained_word_embedding_path.parent / (
-                abs_pretrained_word_embedding_path.stem + ".pt"
-            )
-            pretrained_word_embeddings = load_fasttext(
-                str(abs_pretrained_word_embedding_path),
-                serialized_path,
-                self.preprocessor.get_vocab(),
-                self.preprocessor.pad_token_id,
-            )
-            pretrained_word_embeddings.requires_grad_(requires_grad=False)
-
-        self.tdgu = TDGULightningModule(
-            text_encoder_vocab_size=self.preprocessor.vocab_size,
-            label_head_bos_token_id=self.preprocessor.bos_token_id,
-            label_head_eos_token_id=self.preprocessor.eos_token_id,
-            label_head_pad_token_id=self.preprocessor.pad_token_id,
-            pretrained_word_embeddings=pretrained_word_embeddings,
-            **kwargs,
-        )
+        self.tdgu = TDGULightningModule(**kwargs)
         self.text_decoder = TextDecoder(
             self.tdgu.text_encoder.get_input_embeddings(),
             text_decoder_num_blocks,
@@ -78,45 +41,63 @@ class ObsGenSelfSupervisedTDGU(pl.LightningModule):
             self.tdgu.hparams.hidden_dim,  # type: ignore
         )
 
-        self.ce_loss = nn.CrossEntropyLoss(ignore_index=self.preprocessor.pad_token_id)
+        self.ce_loss = nn.CrossEntropyLoss(
+            ignore_index=self.tdgu.preprocessor.pad_token_id, reduction="none"
+        )
 
         self.val_f1 = F1()
         self.test_f1 = F1()
 
     def forward(  # type: ignore
-        self, step_input: TWCmdGenGraphEventStepInput, batched_graph: Batch
-    ) -> torch.Tensor:
+        self,
+        step_input: TWCmdGenGraphEventStepInput,
+        step_mask: torch.Tensor,
+        prev_batched_graph: Optional[Batch] = None,
+    ) -> Dict[str, Union[torch.Tensor, Batch]]:
         results_list = self.tdgu.greedy_decode(
             step_input,
-            batched_graph,
+            prev_batched_graph
+            if prev_batched_graph is not None
+            else Batch(
+                batch=torch.empty(0, dtype=torch.long),
+                x=torch.empty(0, 0, self.tdgu.preprocessor.vocab_size),
+                node_label_mask=torch.empty(0, 0, dtype=torch.bool),
+                node_last_update=torch.empty(0, 2, dtype=torch.long),
+                edge_index=torch.empty(2, 0, dtype=torch.long),
+                edge_attr=torch.empty(0, 0, self.tdgu.preprocessor.vocab_size),
+                edge_label_mask=torch.empty(0, 0, dtype=torch.bool),
+                edge_last_update=torch.empty(0, 2, dtype=torch.long),
+            ).to(self.device),
             max_event_decode_len=self.tdgu.hparams.max_event_decode_len,  # type: ignore
             max_label_decode_len=self.tdgu.hparams.max_label_decode_len,  # type: ignore
             one_hot=True,
             gumbel_tau=self.gumbel_tau,
         )
 
-        # update graphs in game_id_to_step_data_graph
-        for (game_id, (step_data, _)), updated_graph in zip(
-            self.game_id_to_step_data_graph.items(),
-            batch_to_data_list(
-                results_list[-1]["updated_batched_graph"],
-                step_input.obs_word_ids.size(0),
-            ),
-        ):
-            self.game_id_to_step_data_graph[game_id] = (
-                step_data,
-                updated_graph.detach(),
-            )
-
         # calculate losses with the observation
-        return self.text_decoder(
-            shift_tokens_right(step_input.obs_word_ids, self.preprocessor.bos_token_id),
+        text_decoder_output = self.text_decoder(
+            shift_tokens_right(
+                step_input.obs_word_ids, self.tdgu.preprocessor.bos_token_id
+            ),
             step_input.obs_mask,
             results_list[-1]["batch_node_embeddings"],
             results_list[-1]["batch_node_mask"],
             step_input.prev_action_word_ids,
             step_input.prev_action_mask,
         )
+        loss = torch.flatten(
+            self.ce_loss(
+                text_decoder_output.flatten(end_dim=1),
+                step_input.obs_word_ids.flatten(),
+            ).view_as(step_input.obs_word_ids)
+            * step_mask.unsqueeze(-1)
+        )
+        # (batch * obs_len)
+        return {
+            "text_decoder_output": text_decoder_output,
+            "loss": loss,
+            "updated_batched_graph": results_list[-1]["updated_batched_graph"],
+        }
 
     def on_train_epoch_start(self) -> None:
         self.game_id_to_step_data_graph: Dict[int, Tuple[Dict[str, Any], Data]] = {}
@@ -127,69 +108,24 @@ class ObsGenSelfSupervisedTDGU(pl.LightningModule):
     def on_test_epoch_start(self) -> None:
         self.game_id_to_step_data_graph = {}
 
-    def prepare_greedy_decode_input(
-        self,
-        batch: List[Tuple[int, Dict[str, Any]]],
-        collator: TWCmdGenGraphEventDataCollator,
-    ) -> Tuple[TWCmdGenGraphEventStepInput, Batch]:
-        # finished games are the ones that were in game_id_to_graph, but are not
-        # part of the new batch
-        for finished_game_id in self.game_id_to_step_data_graph.keys() - {
-            game_id for game_id, _ in batch
-        }:
-            self.game_id_to_step_data_graph.pop(finished_game_id)
-
-        # new games are the ones that were not in game_id_to_graph, but are now
-        # part of the new batch.
-        # due to Python's dictionary ordering (insertion order), new games are
-        # added always to the end.
-        for game_id, step_data in batch:
-            if game_id in self.game_id_to_step_data_graph:
-                _, graph = self.game_id_to_step_data_graph[game_id]
-                self.game_id_to_step_data_graph[game_id] = (step_data, graph)
-            else:
-                self.game_id_to_step_data_graph[game_id] = (
-                    step_data,
-                    Data(
-                        x=torch.empty(0, 0, self.preprocessor.vocab_size),
-                        node_label_mask=torch.empty(0, 0).bool(),
-                        node_last_update=torch.empty(0, 2, dtype=torch.long),
-                        edge_index=torch.empty(2, 0, dtype=torch.long),
-                        edge_attr=torch.empty(0, 0, self.preprocessor.vocab_size),
-                        edge_label_mask=torch.empty(0, 0).bool(),
-                        edge_last_update=torch.empty(0, 2, dtype=torch.long),
-                    ).to(self.device),
-                )
-
-        # construct a batch
-        batched_obs: List[str] = []
-        batched_prev_actions: List[str] = []
-        batched_timestamps: List[int] = []
-        graph_list: List[Data] = []
-        for game_id, (step_data, graph) in self.game_id_to_step_data_graph.items():
-            batched_obs.append(step_data["observation"])
-            batched_prev_actions.append(step_data["previous_action"])
-            batched_timestamps.append(step_data["timestamp"])
-            graph_list.append(graph)
-        return collator.collate_step_inputs(
-            batched_obs, batched_prev_actions, batched_timestamps
-        ).to(self.device), data_list_to_batch(graph_list)
-
     def training_step(  # type: ignore
-        self, batch: List[Tuple[int, Dict[str, Any]]], batch_idx: int
-    ) -> torch.Tensor:
-        step_input, batched_graph = self.prepare_greedy_decode_input(
-            batch, self.trainer.datamodule.collator  # type: ignore
-        )
-        text_decoder_output = self(step_input, batched_graph)
-        # (batch, obs_len, num_word)
-        loss = self.ce_loss(
-            text_decoder_output.flatten(end_dim=1), step_input.obs_word_ids.flatten()
-        )
+        self,
+        batch: TWCmdGenObsGenBatch,
+        batch_idx: int,
+        prev_batched_graph: Optional[Batch],
+    ) -> Dict[str, Union[torch.Tensor, Batch]]:
+        losses: List[torch.Tensor] = []
+        for step_input, step_mask in zip(batch.step_inputs, batch.step_mask):
+            # step_mask: (batch)
+            results = self(step_input, step_mask, prev_batched_graph=prev_batched_graph)
+            prev_batched_graph = results["updated_batched_graph"]
+            losses.append(results["loss"])
+        loss = torch.stack(losses).mean()
         self.log("train_loss", loss)
-        return loss
+        assert prev_batched_graph is not None
+        return {"loss": loss, "hiddens": prev_batched_graph.detach()}
 
-    def eval_step(self, batch: List[Tuple[int, Dict[str, Any]]]) -> None:
+    def eval_step(self, batch: TWCmdGenObsGenBatch) -> None:
         if self.trainer.state.stage in {  # type: ignore
             RunningStage.SANITY_CHECKING,
             RunningStage.VALIDATING,
@@ -201,35 +137,53 @@ class ObsGenSelfSupervisedTDGU(pl.LightningModule):
             raise ValueError(
                 f"Unsupported stage: {self.trainer.state.stage}"  # type: ignore
             )
-        step_input, batched_graph = self.prepare_greedy_decode_input(
-            batch, self.trainer.datamodule.collator  # type: ignore
-        )
-        batch_size = step_input.obs_word_ids.size(0)
-        text_decoder_output = self(step_input, batched_graph)
-        loss = self.ce_loss(
-            text_decoder_output.flatten(end_dim=1), step_input.obs_word_ids.flatten()
-        )
-        self.log(f"{log_prefix}_loss", loss, batch_size=batch_size)
-        sizes = step_input.obs_mask.sum(dim=1).tolist()
+        losses: List[torch.Tensor] = []
         f1 = getattr(self, f"{log_prefix}_f1")
-        f1(
-            [
-                ids[:size]
-                for ids, size in zip(text_decoder_output.argmax(dim=-1).tolist(), sizes)
-            ],
-            [ids[:size] for ids, size in zip(step_input.obs_word_ids.tolist(), sizes)],
-        )
+        prev_batched_graph: Optional[Batch] = None
+        for step_input, step_mask in zip(batch.step_inputs, batch.step_mask):
+            results = self(step_input, step_mask, prev_batched_graph=prev_batched_graph)
+            prev_batched_graph = results["updated_batched_graph"]
+            losses.append(results["loss"])
+            sizes = step_input.obs_mask.sum(dim=1).tolist()
+            f1(
+                [
+                    ids[:size]
+                    for ids, size in zip(
+                        results["text_decoder_output"].argmax(dim=-1).tolist(), sizes
+                    )
+                ],
+                [
+                    ids[:size]
+                    for ids, size in zip(step_input.obs_word_ids.tolist(), sizes)
+                ],
+            )
+        loss = torch.cat(losses).mean()
+        batch_size = batch.step_mask.size(1)
+        self.log(f"{log_prefix}_loss", loss, batch_size=batch_size)
         self.log(f"{log_prefix}_f1", f1, batch_size=batch_size)
 
     def validation_step(  # type: ignore
-        self, batch: List[Tuple[int, Dict[str, Any]]], batch_idx: int
+        self, batch: TWCmdGenObsGenBatch, batch_idx: int
     ) -> None:
         self.eval_step(batch)
 
     def test_step(  # type: ignore
-        self, batch: List[Tuple[int, Dict[str, Any]]], batch_idx: int
+        self, batch: TWCmdGenObsGenBatch, batch_idx: int
     ) -> None:
         self.eval_step(batch)
+
+    def tbptt_split_batch(
+        self, batch: TWCmdGenObsGenBatch, split_size: int
+    ) -> List[TWCmdGenObsGenBatch]:
+        splits: List[TWCmdGenObsGenBatch] = []
+        for i in range(0, len(batch.step_inputs), split_size):
+            splits.append(
+                TWCmdGenObsGenBatch(
+                    step_inputs=batch.step_inputs[i : i + split_size],
+                    step_mask=batch.step_mask[i : i + split_size],
+                )
+            )
+        return splits
 
     @property
     def gumbel_tau(self) -> float:
--- tests/train/test_self_supervised.py
+++ tests/train/test_self_supervised.py
@@ -1,12 +1,10 @@
 import pytest
 import torch
-import torch.nn.functional as F
 import shutil
 
-from utils import EqualityData, EqualityBatch
 
 from tdgu.train.self_supervised import ObsGenSelfSupervisedTDGU
-from tdgu.data import TWCmdGenGraphEventStepInput, TWCmdGenGraphEventDataCollator
+from tdgu.data import TWCmdGenGraphEventStepInput, TWCmdGenObsGenBatch
 
 
 @pytest.fixture
@@ -19,237 +17,52 @@ def obs_gen_self_supervised_tdgu(tmp_path):
 
 
 @pytest.mark.parametrize(
-    "init_game_id_to_step_data_graph,batch,expected_step_input,expected_batched_graph,"
-    "expected_game_id_to_step_data_graph",
+    "batch,split_size,expected",
     [
         (
-            {},
+            TWCmdGenObsGenBatch(
+                (TWCmdGenGraphEventStepInput(),), torch.tensor([[True]])
+            ),
+            1,
             [
-                (
-                    0,
-                    {
-                        "observation": "my name is peter",
-                        "previous_action": "chopped east",
-                        "timestamp": 0,
-                    },
-                ),
-                (
-                    1,
-                    {
-                        "observation": "player is chopped",
-                        "previous_action": "player",
-                        "timestamp": 0,
-                    },
-                ),
+                TWCmdGenObsGenBatch(
+                    (TWCmdGenGraphEventStepInput(),), torch.tensor([[True]])
+                )
             ],
-            TWCmdGenGraphEventStepInput(
-                obs_word_ids=torch.tensor([[2, 5, 6, 7, 8, 3], [2, 13, 7, 15, 3, 0]]),
-                obs_mask=torch.tensor([[True] * 6, [True] * 5 + [False]]),
-                prev_action_word_ids=torch.tensor([[2, 15, 16, 3], [2, 13, 3, 0]]),
-                prev_action_mask=torch.tensor([[True] * 4, [True] * 3 + [False]]),
-                timestamps=torch.tensor([0, 0]),
-            ),
-            EqualityBatch(
-                batch=torch.empty(0, dtype=torch.long),
-                x=torch.empty(0, 0, 17),
-                node_label_mask=torch.empty(0, 0).bool(),
-                node_last_update=torch.empty(0, 2, dtype=torch.long),
-                edge_index=torch.empty(2, 0, dtype=torch.long),
-                edge_attr=torch.empty(0, 0, 17),
-                edge_label_mask=torch.empty(0, 0).bool(),
-                edge_last_update=torch.empty(0, 2, dtype=torch.long),
-            ),
-            {
-                0: (
-                    {
-                        "observation": "my name is peter",
-                        "previous_action": "chopped east",
-                        "timestamp": 0,
-                    },
-                    EqualityData(
-                        x=torch.empty(0, 0, 17),
-                        node_label_mask=torch.empty(0, 0).bool(),
-                        node_last_update=torch.empty(0, 2, dtype=torch.long),
-                        edge_index=torch.empty(2, 0, dtype=torch.long),
-                        edge_attr=torch.empty(0, 0, 17),
-                        edge_label_mask=torch.empty(0, 0).bool(),
-                        edge_last_update=torch.empty(0, 2, dtype=torch.long),
-                    ),
-                ),
-                1: (
-                    {
-                        "observation": "player is chopped",
-                        "previous_action": "player",
-                        "timestamp": 0,
-                    },
-                    EqualityData(
-                        x=torch.empty(0, 0, 17),
-                        node_label_mask=torch.empty(0, 0).bool(),
-                        node_last_update=torch.empty(0, 2, dtype=torch.long),
-                        edge_index=torch.empty(2, 0, dtype=torch.long),
-                        edge_attr=torch.empty(0, 0, 17),
-                        edge_label_mask=torch.empty(0, 0).bool(),
-                        edge_last_update=torch.empty(0, 2, dtype=torch.long),
-                    ),
-                ),
-            },
         ),
         (
-            {
-                0: ({}, EqualityData()),
-                1: (
-                    {
-                        "observation": "east of inventory is player",
-                        "previous_action": "to east",
-                        "timestamp": 2,
-                    },
-                    EqualityData(
-                        x=F.one_hot(
-                            torch.tensor([[2, 13, 3], [2, 14, 3]]), num_classes=17
-                        ),
-                        node_label_mask=torch.ones(2, 3).bool(),
-                        node_last_update=torch.tensor([[1, 2], [0, 0]]),
-                        edge_index=torch.tensor([[0], [1]]),
-                        edge_attr=F.one_hot(torch.tensor([[2, 7, 3]]), num_classes=17),
-                        edge_label_mask=torch.ones(1, 3).bool(),
-                        edge_last_update=torch.tensor([[0, 1]]),
-                    ),
-                ),
-                2: ({}, EqualityData()),
-            },
+            TWCmdGenObsGenBatch(
+                (TWCmdGenGraphEventStepInput(),), torch.tensor([[True]])
+            ),
+            2,
             [
+                TWCmdGenObsGenBatch(
+                    (TWCmdGenGraphEventStepInput(),), torch.tensor([[True]])
+                )
+            ],
+        ),
+        (
+            TWCmdGenObsGenBatch(
                 (
-                    1,
-                    {
-                        "observation": "player east is peter",
-                        "previous_action": "inventory",
-                        "timestamp": 3,
-                    },
+                    TWCmdGenGraphEventStepInput(),
+                    TWCmdGenGraphEventStepInput(),
+                    TWCmdGenGraphEventStepInput(),
                 ),
-                (
-                    3,
-                    {
-                        "observation": "my name is peter",
-                        "previous_action": "chopped east",
-                        "timestamp": 0,
-                    },
+                torch.tensor([[True] * 3, [True, False, True], [False, False, True]]),
+            ),
+            2,
+            [
+                TWCmdGenObsGenBatch(
+                    (TWCmdGenGraphEventStepInput(), TWCmdGenGraphEventStepInput()),
+                    torch.tensor([[True] * 3, [True, False, True]]),
                 ),
-                (
-                    4,
-                    {
-                        "observation": "player is chopped",
-                        "previous_action": "player",
-                        "timestamp": 0,
-                    },
+                TWCmdGenObsGenBatch(
+                    (TWCmdGenGraphEventStepInput(),),
+                    torch.tensor([[False, False, True]]),
                 ),
             ],
-            TWCmdGenGraphEventStepInput(
-                obs_word_ids=torch.tensor(
-                    [[2, 13, 16, 7, 8, 3], [2, 5, 6, 7, 8, 3], [2, 13, 7, 15, 3, 0]]
-                ),
-                obs_mask=torch.tensor([[True] * 6, [True] * 6, [True] * 5 + [False]]),
-                prev_action_word_ids=torch.tensor(
-                    [[2, 14, 3, 0], [2, 15, 16, 3], [2, 13, 3, 0]]
-                ),
-                prev_action_mask=torch.tensor(
-                    [[True] * 3 + [False], [True] * 4, [True] * 3 + [False]]
-                ),
-                timestamps=torch.tensor([3, 0, 0]),
-            ),
-            EqualityBatch(
-                batch=torch.tensor([0, 0]),
-                x=F.one_hot(
-                    torch.tensor([[2, 13, 3], [2, 14, 3]]), num_classes=17
-                ).float(),
-                node_label_mask=torch.ones(2, 3).bool(),
-                node_last_update=torch.tensor([[1, 2], [0, 0]]),
-                edge_index=torch.tensor([[0], [1]]),
-                edge_attr=F.one_hot(torch.tensor([[2, 7, 3]]), num_classes=17).float(),
-                edge_label_mask=torch.ones(1, 3).bool(),
-                edge_last_update=torch.tensor([[0, 1]]),
-            ),
-            {
-                1: (
-                    {
-                        "observation": "player east is peter",
-                        "previous_action": "inventory",
-                        "timestamp": 3,
-                    },
-                    EqualityData(
-                        x=F.one_hot(
-                            torch.tensor([[2, 13, 3], [2, 14, 3]]), num_classes=17
-                        ),
-                        node_label_mask=torch.ones(2, 3).bool(),
-                        node_last_update=torch.tensor([[1, 2], [0, 0]]),
-                        edge_index=torch.tensor([[0], [1]]),
-                        edge_attr=F.one_hot(torch.tensor([[2, 7, 3]]), num_classes=17),
-                        edge_label_mask=torch.ones(1, 3).bool(),
-                        edge_last_update=torch.tensor([[0, 1]]),
-                    ),
-                ),
-                3: (
-                    {
-                        "observation": "my name is peter",
-                        "previous_action": "chopped east",
-                        "timestamp": 0,
-                    },
-                    EqualityData(
-                        x=torch.empty(0, 0, 17),
-                        node_label_mask=torch.empty(0, 0).bool(),
-                        node_last_update=torch.empty(0, 2, dtype=torch.long),
-                        edge_index=torch.empty(2, 0, dtype=torch.long),
-                        edge_attr=torch.empty(0, 0, 17),
-                        edge_label_mask=torch.empty(0, 0).bool(),
-                        edge_last_update=torch.empty(0, 2, dtype=torch.long),
-                    ),
-                ),
-                4: (
-                    {
-                        "observation": "player is chopped",
-                        "previous_action": "player",
-                        "timestamp": 0,
-                    },
-                    EqualityData(
-                        x=torch.empty(0, 0, 17),
-                        node_label_mask=torch.empty(0, 0).bool(),
-                        node_last_update=torch.empty(0, 2, dtype=torch.long),
-                        edge_index=torch.empty(2, 0, dtype=torch.long),
-                        edge_attr=torch.empty(0, 0, 17),
-                        edge_label_mask=torch.empty(0, 0).bool(),
-                        edge_last_update=torch.empty(0, 2, dtype=torch.long),
-                    ),
-                ),
-            },
         ),
     ],
 )
-def test_prepare_greedy_decode_input(
-    monkeypatch,
-    obs_gen_self_supervised_tdgu,
-    init_game_id_to_step_data_graph,
-    batch,
-    expected_step_input,
-    expected_batched_graph,
-    expected_game_id_to_step_data_graph,
-):
-    monkeypatch.setattr("tdgu.train.self_supervised.Data", EqualityData)
-    monkeypatch.setattr("tdgu.train.self_supervised.Batch", EqualityBatch)
-    monkeypatch.setattr("tdgu.graph.Data", EqualityData)
-    monkeypatch.setattr("tdgu.graph.Batch", EqualityBatch)
-
-    obs_gen_self_supervised_tdgu.game_id_to_step_data_graph = (
-        init_game_id_to_step_data_graph
-    )
-
-    (
-        step_input,
-        batched_graph,
-    ) = obs_gen_self_supervised_tdgu.prepare_greedy_decode_input(
-        batch, TWCmdGenGraphEventDataCollator(obs_gen_self_supervised_tdgu.preprocessor)
-    )
-    assert step_input == expected_step_input
-    assert batched_graph == expected_batched_graph
-    assert (
-        obs_gen_self_supervised_tdgu.game_id_to_step_data_graph
-        == expected_game_id_to_step_data_graph
-    )
+def test_tbptt_split_batch(obs_gen_self_supervised_tdgu, batch, split_size, expected):
+    assert obs_gen_self_supervised_tdgu.tbptt_split_batch(batch, split_size) == expected
