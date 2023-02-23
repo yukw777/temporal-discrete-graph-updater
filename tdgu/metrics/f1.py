Fix F1 metrics calculation

tdgu/metrics/f1.py
@@ -2,6 +2,7 @@ import torch
 
 from typing import List
 from torchmetrics import Metric
+from torchmetrics.functional.classification import multiclass_f1_score
 
 
 class F1(Metric):
@@ -13,13 +14,16 @@ class F1(Metric):
     higher_is_better = True
     full_state_update = False
 
+    score: torch.Tensor
+    total: torch.Tensor
+
     def __init__(self) -> None:
         super().__init__()
 
         self.add_state("score", default=torch.tensor(0.0), dist_reduce_fx="sum")
         self.add_state("total", default=torch.tensor(0), dist_reduce_fx="sum")
 
-    def update(  # type: ignore
+    def update(
         self, batch_preds: List[List[str]], batch_targets: List[List[str]]
     ) -> None:
         assert len(batch_preds) == len(batch_targets)
@@ -27,7 +31,7 @@ class F1(Metric):
             preds_set = set(preds)
             targets_set = set(targets)
             if preds_set == targets_set:
-                self.score += 1  # type: ignore
+                self.score += 1
             else:
                 matches = 0
                 for pred in preds_set:
@@ -37,12 +41,44 @@ class F1(Metric):
                     # calculate the f1 score
                     precision = matches / len(preds_set)
                     recall = matches / len(targets_set)
-                    self.score += (2 * precision * recall) / (  # type: ignore
-                        precision + recall
-                    )
-            self.total += 1  # type: ignore
+                    self.score += (2 * precision * recall) / (precision + recall)
+            self.total += 1
 
     def compute(self) -> torch.Tensor:
         if self.total == 0:
-            return torch.zeros_like(self.total, dtype=torch.float)  # type: ignore
-        return self.score / self.total  # type: ignore
+            return torch.zeros_like(self.total, dtype=torch.float)
+        return self.score / self.total
+
+
+class DynamicGraphNodeF1(Metric):
+    """
+    Measures the F1 score of dynamic graph nodes where the number of nodes varies.
+    """
+
+    is_differentiable = False
+    higher_is_better = True
+    full_state_update = False
+
+    score: torch.Tensor
+
+    def __init__(self) -> None:
+        super().__init__()
+
+        self.add_state("score", default=torch.tensor([]), dist_reduce_fx="cat")
+
+    def update(self, preds: torch.Tensor, target: torch.Tensor) -> None:
+        """
+        preds: float tensor of shape (batch, num_node) with values in [0, 1]
+        target: integer tensor of shape (batch) with values in [0, num_node)
+        """
+        _, num_node = preds.size()
+        if num_node > 1:
+            # we only care about the F1 scores when we have more than one node
+            scores = multiclass_f1_score(preds, target, num_node, average=None)
+            # (num_node)
+            self.score = torch.cat((self.score, scores))
+
+    def compute(self) -> torch.Tensor:
+        if self.score.size(0) == 0:
+            return torch.tensor(0, device=self.device)
+        return self.score.mean()

tdgu/train/supervised.py
@@ -11,9 +11,9 @@ from pytorch_lightning.loggers import WandbLogger
 from pytorch_lightning.trainer.states import RunningStage
 from torch_geometric.data import Batch, Data
 from torch.utils.data import DataLoader
-from torchmetrics.classification.f_beta import F1Score
+from torchmetrics.classification import MulticlassF1Score
 
-from tdgu.metrics import F1, ExactMatch
+from tdgu.metrics import F1, ExactMatch, DynamicGraphNodeF1
 from tdgu.nn.utils import (
     index_edge_attr,
     masked_log_softmax,
@@ -35,6 +35,7 @@ from tdgu.graph import (
     update_rdf_graph,
 )
 from tdgu.train.common import TDGULightningModule
+from tdgu.constants import EVENT_TYPES
 
 
 class SupervisedTDGU(TDGULightningModule):
@@ -46,14 +47,18 @@ class SupervisedTDGU(TDGULightningModule):
         super().__init__(**kwargs)
         self.criterion = UncertaintyWeightedLoss()
 
-        self.val_event_type_f1 = F1Score()
-        self.val_src_node_f1 = F1Score()
-        self.val_dst_node_f1 = F1Score()
-        self.val_label_f1 = F1Score()
-        self.test_event_type_f1 = F1Score()
-        self.test_src_node_f1 = F1Score()
-        self.test_dst_node_f1 = F1Score()
-        self.test_label_f1 = F1Score()
+        self.val_event_type_f1 = MulticlassF1Score(len(EVENT_TYPES), average="micro")
+        self.val_src_node_f1 = DynamicGraphNodeF1()
+        self.val_dst_node_f1 = DynamicGraphNodeF1()
+        self.val_label_f1 = MulticlassF1Score(
+            self.preprocessor.vocab_size, average="micro"
+        )
+        self.test_event_type_f1 = MulticlassF1Score(len(EVENT_TYPES), average="micro")
+        self.test_src_node_f1 = DynamicGraphNodeF1()
+        self.test_dst_node_f1 = DynamicGraphNodeF1()
+        self.test_label_f1 = MulticlassF1Score(
+            self.preprocessor.vocab_size, average="micro"
+        )
 
         self.val_graph_tf_exact_match = ExactMatch()
         self.val_token_tf_exact_match = ExactMatch()
@@ -191,10 +196,10 @@ class SupervisedTDGU(TDGULightningModule):
         groundtruth_event_dst_mask: (batch)
         groundtruth_event_label_mask: (batch)
         """
-        event_type_f1 = getattr(self, f"{log_prefix}_event_type_f1")
-        src_node_f1 = getattr(self, f"{log_prefix}_src_node_f1")
-        dst_node_f1 = getattr(self, f"{log_prefix}_dst_node_f1")
-        label_f1 = getattr(self, f"{log_prefix}_label_f1")
+        event_type_f1: MulticlassF1Score = getattr(self, f"{log_prefix}_event_type_f1")
+        src_node_f1: DynamicGraphNodeF1 = getattr(self, f"{log_prefix}_src_node_f1")
+        dst_node_f1: DynamicGraphNodeF1 = getattr(self, f"{log_prefix}_dst_node_f1")
+        label_f1: MulticlassF1Score = getattr(self, f"{log_prefix}_label_f1")
 
         event_type_f1.update(
             event_type_logits[groundtruth_event_mask].softmax(dim=1),

tests/train/test_supervised.py
@@ -80,7 +80,9 @@ def test_uncertainty_weighted_loss(
             torch.tensor([[0, 1, 0, 0, 0, 0, 0]]).float(),
             torch.tensor([0]),
             torch.tensor([[False] * 7]),
-            torch.tensor([[[0, 0, 1, 0, 0, 0, 0], [0, 0, 0, 1, 0, 0, 0]]]).float(),
+            torch.tensor(
+                [[[0] * 2 + [1] + [0] * 14, [0] * 3 + [1] + [0] * 13]]
+            ).float(),
             torch.tensor([[2, 3]]),
             torch.ones(1, 2).bool(),
             torch.tensor([True]),
@@ -100,7 +102,9 @@ def test_uncertainty_weighted_loss(
             torch.tensor([[0, 1, 0, 0, 0, 0, 0]]).float(),
             torch.tensor([0]),
             torch.tensor([[False] * 7]),
-            torch.tensor([[[0, 0, 1, 0, 0, 0, 0], [0, 0, 0, 1, 0, 0, 0]]]).float(),
+            torch.tensor(
+                [[[0] * 2 + [1] + [0] * 14, [0] * 3 + [1] + [0] * 13]]
+            ).float(),
             torch.tensor([[2, 3]]),
             torch.ones(1, 2).bool(),
             torch.tensor([True]),
@@ -120,7 +124,9 @@ def test_uncertainty_weighted_loss(
             torch.tensor([[0, 1, 0, 0, 0, 0, 0]]).float(),
             torch.tensor([0]),
             torch.tensor([[False] * 7]),
-            torch.tensor([[[0, 0, 1, 0, 0, 0, 0], [0, 0, 0, 1, 0, 0, 0]]]).float(),
+            torch.tensor(
+                [[[0] * 2 + [1] + [0] * 14, [0] * 3 + [1] + [0] * 13]]
+            ).float(),
             torch.tensor([[2, 3]]),
             torch.ones(1, 2).bool(),
             torch.tensor([True]),
@@ -140,7 +146,9 @@ def test_uncertainty_weighted_loss(
             torch.tensor([[0, 1, 0, 0, 0, 0, 0]]).float(),
             torch.tensor([0]),
             torch.tensor([[False] * 7]),
-            torch.tensor([[[0, 1, 0, 0, 0, 0, 0], [0, 0, 1, 0, 0, 0, 0]]]).float(),
+            torch.tensor(
+                [[[0] * 1 + [1] + [0] * 15, [0] * 2 + [1] + [0] * 14]]
+            ).float(),
             torch.tensor([[2, 3]]),
             torch.ones(1, 2).bool(),
             torch.tensor([True]),
@@ -160,7 +168,9 @@ def test_uncertainty_weighted_loss(
             torch.tensor([[1, 0, 0, 0, 0, 0, 0]]).float(),
             torch.tensor([0]),
             torch.tensor([[True, True, True] + [False] * 4]),
-            torch.tensor([[[0, 0, 1, 0, 0, 0, 0], [0, 0, 0, 1, 0, 0, 0]]]).float(),
+            torch.tensor(
+                [[[0] * 2 + [1] + [0] * 14, [0] * 3 + [1] + [0] * 13]]
+            ).float(),
             torch.tensor([[2, 3]]),
             torch.ones(1, 2).bool(),
             torch.tensor([True]),
@@ -168,7 +178,7 @@ def test_uncertainty_weighted_loss(
             torch.tensor([False]),
             torch.tensor([False]),
             1,
-            1,
+            1 / 7,
             0,
             0,
         ),
@@ -180,7 +190,9 @@ def test_uncertainty_weighted_loss(
             torch.tensor([[0, 1, 0, 0, 0, 0, 0]]).float(),
             torch.tensor([0]),
             torch.tensor([[True, True, True] + [False] * 4]),
-            torch.tensor([[[0, 0, 1, 0, 0, 0, 0], [0, 0, 0, 1, 0, 0, 0]]]).float(),
+            torch.tensor(
+                [[[0] * 2 + [1] + [0] * 14, [0] * 3 + [1] + [0] * 13]]
+            ).float(),
             torch.tensor([[2, 3]]),
             torch.ones(1, 2).bool(),
             torch.tensor([True]),
@@ -200,7 +212,9 @@ def test_uncertainty_weighted_loss(
             torch.tensor([[0, 0, 0, 1, 0, 0, 0]]).float(),
             torch.tensor([3]),
             torch.tensor([[True, True, True, True, True, False, False]]),
-            torch.tensor([[[0, 0, 1, 0, 0, 0, 0], [0, 0, 0, 1, 0, 0, 0]]]).float(),
+            torch.tensor(
+                [[[0] * 2 + [1] + [0] * 14, [0] * 3 + [1] + [0] * 13]]
+            ).float(),
             torch.tensor([[2, 3]]),
             torch.ones(1, 2).bool(),
             torch.tensor([True]),
@@ -208,8 +222,8 @@ def test_uncertainty_weighted_loss(
             torch.tensor([True]),
             torch.tensor([True]),
             1,
-            1,
-            1,
+            1 / 7,
+            1 / 7,
             1,
         ),
         (
@@ -220,7 +234,9 @@ def test_uncertainty_weighted_loss(
             torch.tensor([[0, 1, 0, 0, 0, 0, 0]]).float(),
             torch.tensor([0]),
             torch.tensor([[True, True, True, True, True, False, False]]),
-            torch.tensor([[[0, 1, 0, 0, 0, 0, 0], [0, 0, 1, 0, 0, 0, 0]]]).float(),
+            torch.tensor(
+                [[[0] * 1 + [1] + [0] * 15, [0] * 2 + [1] + [0] * 14]]
+            ).float(),
             torch.tensor([[2, 3]]),
             torch.ones(1, 2).bool(),
             torch.tensor([True]),
@@ -240,7 +256,9 @@ def test_uncertainty_weighted_loss(
             torch.tensor([[0, 0, 0, 1, 0, 0, 0]]).float(),
             torch.tensor([3]),
             torch.tensor([[True, True, True, True, True, False, False]]),
-            torch.tensor([[[0, 0, 1, 0, 0, 0, 0], [0, 0, 0, 1, 0, 0, 0]]]).float(),
+            torch.tensor(
+                [[[0] * 2 + [1] + [0] * 14, [0] * 3 + [1] + [0] * 13]]
+            ).float(),
             torch.tensor([[2, 3]]),
             torch.ones(1, 2).bool(),
             torch.tensor([True]),
@@ -248,8 +266,8 @@ def test_uncertainty_weighted_loss(
             torch.tensor([True]),
             torch.tensor([False]),
             1,
-            1,
-            1,
+            1 / 7,
+            1 / 7,
             0,
         ),
         (
@@ -260,7 +278,9 @@ def test_uncertainty_weighted_loss(
             torch.tensor([[0, 1, 0, 0, 0, 0, 0]]).float(),
             torch.tensor([0]),
             torch.tensor([[True, True, True, True, True, False, False]]),
-            torch.tensor([[[0, 1, 0, 0, 0, 0, 0], [0, 0, 1, 0, 0, 0, 0]]]).float(),
+            torch.tensor(
+                [[[0] * 1 + [1] + [0] * 15, [0] * 2 + [1] + [0] * 14]]
+            ).float(),
             torch.tensor([[2, 3]]),
             torch.ones(1, 2).bool(),
             torch.tensor([True]),
@@ -357,18 +377,18 @@ def test_uncertainty_weighted_loss(
             ),
             torch.tensor(
                 [
-                    [[0, 0, 1, 0, 0, 0, 0, 0], [0, 0, 0, 1, 0, 0, 0, 0]],
-                    [[0, 0, 0, 1, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 1]],
-                    [[0, 0, 0, 0, 0, 1, 0, 0], [0, 0, 0, 0, 1, 0, 0, 0]],
-                    [[0, 0, 1, 0, 0, 0, 0, 0], [0, 0, 0, 0, 1, 0, 0, 0]],
-                    [[0, 0, 0, 1, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 1, 0]],
-                    [[0, 0, 0, 0, 0, 0, 1, 0], [0, 0, 0, 0, 0, 1, 0, 0]],
-                    [[0, 0, 0, 0, 0, 0, 0, 1], [0, 0, 0, 1, 0, 0, 0, 0]],
-                    [[0, 0, 0, 0, 0, 1, 0, 0], [0, 0, 0, 1, 0, 0, 0, 0]],
-                    [[0, 0, 0, 0, 1, 0, 0, 0], [0, 0, 1, 0, 0, 0, 0, 0]],
-                    [[0, 0, 0, 0, 0, 1, 0, 0], [0, 0, 0, 0, 0, 0, 1, 0]],
-                    [[0, 0, 0, 1, 0, 0, 0, 0], [0, 0, 0, 0, 0, 1, 0, 0]],
-                    [[0, 0, 0, 0, 1, 0, 0, 0], [0, 0, 0, 1, 0, 0, 0, 0]],
+                    [[0] * 2 + [1] + [0] * 14, [0] * 3 + [1] + [0] * 13],
+                    [[0] * 3 + [1] + [0] * 13, [0] * 7 + [1] + [0] * 9],
+                    [[0] * 5 + [1] + [0] * 11, [0] * 4 + [1] + [0] * 12],
+                    [[0] * 2 + [1] + [0] * 14, [0] * 4 + [1] + [0] * 12],
+                    [[0] * 3 + [1] + [0] * 13, [0] * 6 + [1] + [0] * 10],
+                    [[0] * 6 + [1] + [0] * 10, [0] * 5 + [1] + [0] * 11],
+                    [[0] * 7 + [1] + [0] * 9, [0] * 3 + [1] + [0] * 13],
+                    [[0] * 5 + [1] + [0] * 11, [0] * 3 + [1] + [0] * 13],
+                    [[0] * 4 + [1] + [0] * 12, [0] * 2 + [1] + [0] * 14],
+                    [[0] * 5 + [1] + [0] * 11, [0] * 6 + [1] + [0] * 10],
+                    [[0] * 3 + [1] + [0] * 13, [0] * 5 + [1] + [0] * 11],
+                    [[0] * 4 + [1] + [0] * 12, [0] * 3 + [1] + [0] * 13],
                 ]
             ).float(),
             torch.tensor(
@@ -453,8 +473,8 @@ def test_uncertainty_weighted_loss(
                 ]
             ),
             1,
-            1,
-            1,
+            3 / 8,
+            1 / 8,
             1,
         ),
     ],